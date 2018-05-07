e0d1c915ad7005b5568b5ffd2baed5e846c7aef4
tkgsy <meirvg@gmail.com>
2018-05-07 17:23:44 +0900

    Modified to use extern package instead of submodule

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 // https://www.cisco.com/en/US/technologies/tk648/tk362/technologies_white_paper09186a00800a3db9.html
-extern crate netflood;
+extern crate netflow;
+extern crate serde_json;
 
-use netflood::netflow;
+use std::io::BufReader;
+use std::fs::File;
 
 fn main() {
-    println!("{:?}", netflow::field::FieldTypes::IN_BYTES);
+    let bufr = BufReader::new(
+        File::open(
+            "/home/tkgsy/misc/scripts/netflood/netflood/template/template.json",
+        ).unwrap(),
+    );
+
+    // let mut template = String::new();
+    // bufr.read_to_string(&mut template).unwrap();
+
+    let val: serde_json::Value = serde_json::from_reader(bufr).unwrap();
+
+    println!("{:?}", val);
+
+    println!("{:?}", netflow::field::FieldTypes::DST_AS);
 }
