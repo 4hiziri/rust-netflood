dca0946f6711051ca41413cdfcb737eaea18de0c
tkgsy <meirvg@gmail.com>
2018-05-08 19:18:10 +0900

    Add new module

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 extern crate netflow;
+extern crate pcapng;
 extern crate serde_json;
+extern crate pnet;
 
-// pub mod pcap_analysis;
+pub mod pcap_analysis;
 pub mod template_parser;
 // pub mod flow_generate;
