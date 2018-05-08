c18297e13f6a74f610623155fc43e703bd1fd68c
tkgsy <meirvg@gmail.com>
2018-05-08 13:28:35 +0900

    Add initialize test code

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,26 @@
 // https://www.cisco.com/en/US/technologies/tk648/tk362/technologies_white_paper09186a00800a3db9.html
 extern crate netflow;
 extern crate serde_json;
+extern crate netflood;
 
 use std::io::BufReader;
 use std::fs::File;
 
+use netflood::template_parser;
+
 fn main() {
     let bufr = BufReader::new(
         File::open(
-            "/home/tkgsy/misc/scripts/netflood/netflood/template/template.json",
+            "/home/tkgsy/misc/scripts/netflood/netflood/rsc/template/template.json",
         ).unwrap(),
     );
-
-    // let mut template = String::new();
-    // bufr.read_to_string(&mut template).unwrap();
 
     let val: serde_json::Value = serde_json::from_reader(bufr).unwrap();
 
     println!("{:?}", val);
 
-    println!("{:?}", netflow::field::FieldTypes::DST_AS);
+    println!(
+        "{:?}",
+        template_parser::from_str("{\"name\": \"John Doe\"}")
+    );
 }
