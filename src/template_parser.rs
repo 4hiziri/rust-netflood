7e90aba3d9a0c17ba92a0c2bf8c49c58f54d2037
tkgsy <meirvg@gmail.com>
2018-05-08 19:18:39 +0900

    Add new inteface

--- src/template_parser.rs
+++ src/template_parser.rs
@@ -1,7 +1,17 @@
 use netflow::flowset::DataTemplate;
 use serde_json;
+use std::io::Read;
 
-pub fn from_str(template_str: &str) -> DataTemplate {
-    let val: serde_json::Value = serde_json::from_str(template_str).unwrap();
-    DataTemplate::new()
+pub fn from_str(template_str: &str) -> Result<DataTemplate, serde_json::Error> {
+    match serde_json::from_str::<serde_json::Value>(template_str) {
+        Ok(_val) => Ok(DataTemplate::new()),
+        Err(e) => Err(e),
+    }
+}
+
+pub fn from_reader(template_reader: &mut Read) -> Result<DataTemplate, serde_json::Error> {
+    match serde_json::from_reader::<&mut Read, serde_json::Value>(template_reader) {
+        Ok(_val) => Ok(DataTemplate::new()),
+        Err(e) => Err(e),
+    }
 }
