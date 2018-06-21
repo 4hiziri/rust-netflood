41091b66baedc542cbdd1b0042cf39b1f527b7e3
tkgsy <meirvg@gmail.com>
2018-06-21 10:46:56 +0900

    Impl dataflow_generator based on OptionTemplate

--- src/flow_generator.rs
+++ src/flow_generator.rs
@@ -1,22 +1,31 @@
 use generate_rand::rand_field;
-use netflow::flowset::DataFlow;
-use netflow::flowset::DataTemplateItem;
+use netflow::field::TypeLengthField;
+use netflow::flowset::{DataFlow, DataTemplateItem, OptionTemplateItem};
 
-pub fn from_template(template: DataTemplateItem, count: usize) -> Vec<DataFlow> {
+fn from_fields(id: u16, fields: &Vec<TypeLengthField>, count: usize) -> Vec<DataFlow> {
     let mut dataflow = Vec::with_capacity(count);
-    let template_id = template.template_id;
-    let template = &template.fields;
 
     for _ in 0..count {
         let mut random_data = Vec::with_capacity(count);
 
         for _ in 0..count {
-            random_data.push(rand_field(template));
+            random_data.push(rand_field(fields));
         }
 
         // TODO: wait for impl length method
-        dataflow.push(DataFlow::new(template_id, 0, random_data));
+        dataflow.push(DataFlow::new(id, 0, random_data));
     }
 
     dataflow
+}
+
+pub fn from_template(template: DataTemplateItem, count: usize) -> Vec<DataFlow> {
+    self::from_fields(template.template_id, &template.fields, count)
+}
+
+pub fn from_option(option: OptionTemplateItem, count: usize) -> Vec<DataFlow> {
+    let mut template = option.scopes.clone();
+    template.append(&mut option.options.clone());
+
+    self::from_fields(option.template_id, &template, count)
 }
